d7e32b8f07f6002ac1f4e1471e78b0693f69aed2
Milan Sony <milansonyofficial@gmail.com>
2024-02-23 00:47:57 +0530

    user login and signup function created

--- helpers/product_helpers.js
+++ helpers/product_helpers.js
@@ -4,9 +4,8 @@ const collections = require('../config/collections')
 module.exports = {
 
     addProduct: (product, callback) => {
-        // console.log(product)
+
         db.collection('products').insertOne(product).then((data) => {
-            // console.log(data)
             callback(data.insertedId)
         })
     },
@@ -15,7 +14,6 @@ module.exports = {
         return new Promise(async (resolve, reject) => {
 
             let products = await db.collection(collections.PRODUCT_COLLECTIONS).find().toArray()
-
             resolve(products)
 
         })
---
+++ helpers/user_helpers.js
@@ -0,0 +1,36 @@
+const db = require('../config/connection')
+const collections = require('../config/collections')
+const bcrypt = require('bcrypt')
+
+module.exports = {
+
+    doSignup: (userData) => {
+        return new Promise(async (resolve, reject) => {
+
+            userData.password = await bcrypt.hash(userData.password, 10)
+            db.collection(collections.USER_COLLECTIONS).insertOne(userData).then((data) => {
+                resolve(data)
+            })
+        })
+    },
+
+    doLogin: (userData) => {
+        return new Promise(async (resolve, reject) => {
+            // let loginStatus = false
+            // let response = {}
+            let user = await db.collection(collections.USER_COLLECTIONS).findOne({ email: userData.email })
+            if (user) {
+                bcrypt.compare(userData.password, user.password).then((status) => {
+                    if (status) {
+                        console.log("Login Success")
+                    } else {
+                        console.log("Login Failed")
+                    }
+                })
+            } else {
+                console.log('Login failed')
+            }
+        })
+    }
+
+}
